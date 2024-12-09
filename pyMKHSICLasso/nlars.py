075447c59c9cc2170f403266e64a450c96aaea9f
Xiaowen Li <lxw243@gmail.com>
2024-12-09 22:14:50 +1300

    Update nlars.py

--- pyMKHSICLasso/nlars.py
+++ pyMKHSICLasso/nlars.py
@@ -1,58 +1,62 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from __future__ import (absolute_import, division, print_function,
-                        unicode_literals)
+"""
+Python implementation of the Nonnegative LARS (Least Angle Regression and Selection) solver.
+This script solves the problem:
+    argmin_beta (1/2) ||y - X*beta||_2^2  subject to beta >= 0.
 
+It also computes the entire regularization path for the LASSO problem:
+    min (1/2) ||y - X*beta||_2^2 + lambda * |beta|_1  subject to beta >= 0.
+
+"""
+
+from __future__ import absolute_import, division, print_function, unicode_literals
 from builtins import range
-
 from future import standard_library
-
 import numpy as np
 from scipy.sparse import lil_matrix
+import logging
 
+# Enable Python 3 compatibility
 standard_library.install_aliases()
-
 
 def nlars(X, X_ty, num_feat, max_neighbors):
     """
-    We used the a Python implementation of the Nonnegative LARS solver
-    written in MATLAB at http://orbit.dtu.dk/files/5618980/imm5523.zip
-    Solves the problem argmin_beta 1/2||y-X*beta||_2^2  s.t. beta>=0.
-    The problem is solved using a modification of the Least Angle Regression
-    and Selection algorithm.
-    As such the entire regularization path for the LASSO problem
-    min 1/2||y-X*beta||_2^2 + lambda|beta|_1  s.t. beta>=0
-    for all values of lambda is given in path.
-    Input:
-        X            matrix of size D x D
-        X_ty         vector of size D x 1
-        num_feat     the number of features you want to extract
-    Output:
-        path         the entire solution path
-        beta         D x 1 solution vector
-        A            selected features
-        A_neighbors  related features of the selected features in A
-        lam(lambda)  regularization value at beginning of step corresponds
-                     to value of negative gradient
+    Nonnegative LARS solver for feature selection.
+
+    Parameters:
+    - X: (n, d) matrix, feature matrix.
+    - X_ty: (d, 1) vector, X^T * y.
+    - num_feat: Number of features to extract.
+    - max_neighbors: Maximum number of neighbors for each selected feature.
+
+    Returns:
+    - A_all: List of active feature sets at each step.
+    - path_final: Full regularization path for beta.
+    - beta: (d, 1) vector, final solution for beta.
+    - A_sorted: Selected features, sorted by their beta values.
+    - lam_final: Regularization values at each step.
+    - A_neighbors: Neighbors of the selected features.
+    - A_neighbors_score: Scores of the neighbors.
     """
-    n, d = X.shape
+    n, d = X.shape  # Number of samples (n) and features (d)
 
-    A = []
-    A_all = [] # A_all to record A in each path
-    A_neighbors = []
-    A_neighbors_score = []
-    beta = np.zeros((d, 1), dtype=np.float32)
-    path_all = lil_matrix((d, 4 * d))
-    lam = np.zeros((1, 4 * d))
+    # Initialization
+    A = []  # Active set of selected features
+    A_all = []  # Records active features at each step
+    A_neighbors = []  # Neighbors of selected features
+    A_neighbors_score = []  # Scores of neighbors
+    beta = np.zeros((d, 1), dtype=np.float32) 
+    path_all = lil_matrix((d, 4 * d))  # Path matrix
+    lam = np.zeros((1, 4 * d))  # Regularization values
 
-    I = list(range(d))
-
-    XtXbeta = np.dot(X.transpose(), np.dot(X, beta))
-    c = X_ty - XtXbeta
+    I = list(range(d))  # inactive set
+    XtXbeta = np.dot(X.T, np.dot(X, beta))
+    c = X_ty - XtXbeta  # Residual correlation
     j = c.argmax()
     C = c[j]
-    A.append(I[j])
+    A.append(I[j])  # Add feature with max correlation to active set
     I.remove(I[j])
 
     if len(C) == 0:
@@ -61,24 +65,24 @@ def nlars(X, X_ty, num_feat, max_neighbors):
         lam[0, 0] = C[0]
 
     k = 0
-    while sum(c[A]) / len(A) >= 1e-9 and len(A) < num_feat + 1:
-    #while len(A) < num_feat + 1:
+    while sum(c[A]) / len(A) >= 1e-16 and len(A) < num_feat + 1:
         s = np.ones((len(A), 1), dtype=np.float32)
 
         try:
-            w = np.linalg.solve(np.dot(X[:, A].transpose(), X[:, A]), s)
-        except np.linalg.linalg.LinAlgError:
-            # matrix is singular
-            X_noisy = X[:, A] + np.random.normal(0, 10e-10, X[:, A].shape)
-            w = np.linalg.solve(np.dot(X_noisy.transpose(), X_noisy), s) 
+            # Solve for direction vector w
+            w = np.linalg.solve(np.dot(X[:, A].T, X[:, A]), s)
+        except np.linalg.LinAlgError:
+            # Add noise if matrix is singular
+            X_noisy = X[:, A] + np.random.normal(0, 1e-10, X[:, A].shape)
+            w = np.linalg.solve(np.dot(X_noisy.T, X_noisy), s)
 
-        XtXw = np.dot(X.transpose(), np.dot(X[:, A], w))
+        XtXw = np.dot(X.T, np.dot(X[:, A], w))
 
+        # Compute step sizes
         gamma1 = (C - c[I]) / (XtXw[A[0]] - XtXw[I])
-        gamma2 = -beta[A] / (w)
-        gamma3 = np.zeros((1, 1))
-        gamma3[0] = c[A[0]] / (XtXw[A[0]])
-        gamma = np.concatenate((np.concatenate((gamma1, gamma2)), gamma3))
+        gamma2 = -beta[A] / w
+        gamma3 = c[A[0]] / XtXw[A[0]]
+        gamma = np.concatenate((np.concatenate((gamma1, gamma2)), [gamma3]))
 
         gamma[gamma <= 1e-9] = np.inf
         t = gamma.argmin()
@@ -86,15 +90,17 @@ def nlars(X, X_ty, num_feat, max_neighbors):
 
         beta[A] = beta[A] + mu * w
 
+        # Check if we need to remove a feature
         if t >= len(gamma1) and t < (len(gamma1) + len(gamma2)):
-            lasso_cond = 1
+            lasso_cond = True
             j = t - len(gamma1)
             I.append(A[j])
             A.remove(A[j])
         else:
-            lasso_cond = 0
+            lasso_cond = False
 
-        XtXbeta = np.dot(X.transpose(), np.dot(X, beta))
+        # Update residuals and active set
+        XtXbeta = np.dot(X.T, np.dot(X, beta))
         c = X_ty - XtXbeta
         j = np.argmax(c[I])
         C = max(c[I])
@@ -102,39 +108,31 @@ def nlars(X, X_ty, num_feat, max_neighbors):
         k += 1
         path_all[:, k] = beta
 
-        if len(C) == 0:
-            lam[k] = 0
-        else:
-            lam[0, k] = C[0]
-        if lasso_cond == 0:
+        lam[0, k] = C[0] if len(C) != 0 else 0
+
+        if not lasso_cond:
             A.append(I[j])
             I.remove(I[j])
+
         A_all.append(A[:])
 
-    # We run numfeat + 1 iteration to update beta and path information
-    # Then, we return only numfeat features
+    # Truncate to desired number of features
     if len(A) > num_feat:
         A.pop()
 
-    # Sort A with respect to beta
-    s = beta[A]
-    sort_index = sorted(range(len(s)), key=lambda k: s[k], reverse=True)
+    # Sort active set by beta values
+    A_sorted = sorted(A, key=lambda i: beta[i], reverse=True)
 
-    A_sorted = [A[i] for i in sort_index]
-
-    # Find nighbors of selected features
-    XtXA = np.dot(X.transpose(), X[:, A_sorted])
-
-    # Search up to 10 nighbors
+    # Find neighbors of selected features
+    XtXA = np.dot(X.T, X[:, A_sorted])
     num_neighbors = max_neighbors + 1
-    for i in range(0, len(A_sorted)):
+    for i in range(len(A_sorted)):
         tmp = XtXA[:, i]
-        sort_index = sorted(
-            range(len(tmp)), key=lambda k: tmp[k], reverse=True)
-        A_neighbors.append(sort_index[0:num_neighbors])
-        A_neighbors_score.append(tmp[sort_index[0:num_neighbors]])
+        sort_index = np.argsort(tmp)[::-1]
+        A_neighbors.append(sort_index[:num_neighbors])
+        A_neighbors_score.append(tmp[sort_index[:num_neighbors]])
 
-    path_final = path_all[:, 0:(k + 1)].toarray()
-    lam_final = lam[0:(k + 1)]
+    path_final = path_all[:, :k + 1].toarray()
+    lam_final = lam[:, :k + 1]
 
     return A_all, path_final, beta, A_sorted, lam_final, A_neighbors, A_neighbors_score
